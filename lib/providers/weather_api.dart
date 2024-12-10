dca39b8c3fe1b95b30b78ad954e8733746008e7b
Emre Yurtseven <emreyurtseven18@gmail.com>
2024-12-10 21:10:38 +0100

    improve

--- lib/providers/weather_api.dart
+++ lib/providers/weather_api.dart
@@ -1,11 +1,8 @@
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherApi {
-  WeatherApi(YumemiWeather weather) {
-    weatherInstance = weather;
-  }
-
-  late YumemiWeather weatherInstance;
+  WeatherApi(YumemiWeather weather) : weatherInstance = weather;
+  final YumemiWeather weatherInstance;
 
   String fetchSimpleWeather() {
     final weatherCondition = weatherInstance.fetchSimpleWeather();
